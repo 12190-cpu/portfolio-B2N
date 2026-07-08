78643d70bb58f73e56a2911b67ede8614ba550a8
12190-cpu <12190@holbertonstudents.com>
2026-07-08 20:22:19 +0200

    Fixing frontend

--- frontend/src/pages/Sectors.jsx
+++ frontend/src/pages/Sectors.jsx
@@ -1,45 +1,5 @@
 import { useState } from "react";
-
-const sectors = [
-  {
-    name: "Nord IDF",
-    image: "/images/secteurs/nord.jpg",
-    cities: [
-      "Cergy", "Pontoise", "Herblay", "Osny", "Eragny",
-      "Conflans-Sainte-Honorine", "Saint-Ouen-l'Aumône",
-      "Montigny-lès-Cormeilles", "Vauréal", "Jouy-le-Moutier",
-      "Beauchamp", "Pierrelaye", "Le Plessis-Bouchard",
-      "Courdimanche", "Boisemont", "Maurecourt"
-    ],
-    whatsapp: "https://wa.me/33600000000",
-    snapchat: "https://www.snapchat.com/add/burger2nuit.nord",
-    phone: "tel:+33600000000"
-  },
-  {
-    name: "Nord Est IDF",
-    image: "/images/secteurs/nord-est.jpg",
-    cities: [
-      "Roissy", "Sevran", "Aulnay", "Tremblay",
-      "Villepinte", "Goussainville", "Mitry-Mory", "Le Mesnil-Amelot"
-    ],
-    whatsapp: "https://wa.me/33600000000",
-    snapchat: "https://www.snapchat.com/add/burger2nuit.nordest",
-    phone: "tel:+33600000000"
-  },
-  {
-    name: "Ouest IDF",
-    image: "/images/secteurs/ouest.jpg",
-    cities: [
-      "Clichy", "Levallois", "Asnières-sur-Seine", "Gennevilliers",
-      "Villeneuve-la-Garenne", "Colombes", "Bois-Colombes",
-      "La Garenne-Colombes", "Nanterre", "Rueil-Malmaison",
-      "Suresnes", "Puteaux", "Neuilly-sur-Seine"
-    ],
-    whatsapp: "https://wa.me/33600000000",
-    snapchat: "https://www.snapchat.com/add/burger2nuit.ouest",
-    phone: "tel:+33600000000"
-  }
-];
+import sectors from "../data/sectors";
 
 function Sectors() {
   const [search, setSearch] = useState("");
